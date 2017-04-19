4fa356da6921a1922abf8973558c39e267ae22b1
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-19 19:08:43 +0200

    Initial skeleton commit

---
+++ GitSuggest.CommandLine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GitSuggest.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => MainAsync(args)).Wait();
+            Console.WriteLine("done");
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("before");
+            await Task.Delay(1000);
+            Console.WriteLine("after");
+        }
+    }
+}
---
+++ GitSuggest/SuggestionEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander;
+using Commander.Events;
+using Commander.Monitors;
+
+namespace GitSuggest
+{
+    public class SuggestionEngine
+    {
+        private readonly string _RepositoryFolder;
+
+        public SuggestionEngine(string repositoryFolder)
+        {
+            _RepositoryFolder = repositoryFolder ?? throw new ArgumentNullException(nameof(repositoryFolder));
+        }
+
+        private async Task<(int errorcode, List<string> output)> ExecuteGit(string arguments)
+        {
+            var monitor = new ProcessCollectOutputMonitor();
+            int exitcode = await ConsoleProcess.ExecuteAsync("git.exe", arguments, _RepositoryFolder, monitor);
+            return (exitcode, monitor.Events.OfType<ProcessOutputEvent>().Select(e => e.Line).ToList());
+        }
+
+        public async Task<bool> IsValidGitRepository()
+        {
+            var (exitcode, _) = await ExecuteGit("status");
+            return exitcode == 0;
+        }
+    }
+}
