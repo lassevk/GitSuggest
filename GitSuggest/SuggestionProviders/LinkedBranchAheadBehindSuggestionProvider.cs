fb2a3127f8963d322c1d3776a01de686cc3340fe
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-23 18:57:46 +0200

    Added initial handling of merge, bisect, and cherrypicking

--- GitSuggest/SuggestionProviders/LinkedBranchAheadBehindSuggestionProvider.cs
+++ GitSuggest/SuggestionProviders/LinkedBranchAheadBehindSuggestionProvider.cs
@@ -31,7 +31,7 @@ namespace GitSuggest.SuggestionProviders
 
         public async Task<List<Suggestion>> GetSuggestions(GitRepository repository, IConfiguration configuration)
         {
-            if ((await repository.GetFileStatus()).Count > 0)
+            if (!(await repository.IsCleanWorkingFolder()))
                 return new List<Suggestion>();
 
             var currentBranch = await repository.GetCurrentBranch();
