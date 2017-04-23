a65f6de7f1afca3e0c12917bad94b91530cdc216
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-23 18:43:08 +0200

    Fixed link branches in terms of terminology and merging into current branch

--- GitSuggest/SuggestionProviders/LinkedBranchAheadBehindSuggestionProvider.cs
+++ GitSuggest/SuggestionProviders/LinkedBranchAheadBehindSuggestionProvider.cs
@@ -9,22 +9,24 @@ namespace GitSuggest.SuggestionProviders
     internal abstract class LinkedBranchAheadBehindSuggestionProvider : ISuggestionProvider
     {
         [NotNull]
+        private readonly string _BranchType;
+
+        [NotNull]
         private readonly Predicate<string> _BranchNamePredicate;
 
         [NotNull]
         private readonly string _TargetBranch;
 
         private readonly int _Rank;
+        private readonly bool _AllowMergeToCurrent;
 
-        internal LinkedBranchAheadBehindSuggestionProvider([NotNull] Predicate<string> branchNamePredicate, [NotNull] string targetBranch, int rank)
+        internal LinkedBranchAheadBehindSuggestionProvider([NotNull] string branchType, [NotNull] Predicate<string> branchNamePredicate, [NotNull] string targetBranch, int rank, bool allowMergeToCurrent)
         {
-            _BranchNamePredicate = branchNamePredicate;
-            _TargetBranch = targetBranch;
+            _BranchType = branchType ?? throw new ArgumentNullException(nameof(branchType));
+            _BranchNamePredicate = branchNamePredicate ?? throw new ArgumentNullException(nameof(branchNamePredicate));
+            _TargetBranch = targetBranch ?? throw new ArgumentNullException(nameof(targetBranch));
             _Rank = rank;
-            if (branchNamePredicate == null)
-                throw new ArgumentNullException(nameof(branchNamePredicate));
-            if (targetBranch == null)
-                throw new ArgumentNullException(nameof(targetBranch));
+            _AllowMergeToCurrent = allowMergeToCurrent;
         }
 
         public async Task<List<Suggestion>> GetSuggestions(GitRepository repository, IConfiguration configuration)
@@ -44,9 +46,9 @@ namespace GitSuggest.SuggestionProviders
                 return new List<Suggestion>();
 
             var suggestions = new List<Suggestion>();
-            if (behind > 0)
+            if (behind > 0 && _AllowMergeToCurrent)
             {
-                suggestions.Add(new Suggestion(_Rank, $"Feature-branch '{currentBranch}' is {behind} commit{(behind != 1 ? "s" : "")} behind '{_TargetBranch}'",
+                suggestions.Add(new Suggestion(_Rank, $"{_BranchType} '{currentBranch}' is {behind} commit{(behind != 1 ? "s" : "")} behind '{_TargetBranch}'",
                                                $"This means that other changes have been applied to '{_TargetBranch}' that has not yet been incorporated into '{currentBranch}'",
                                                new SuggestedAction($"View a diff between '{currentBranch}' and '{_TargetBranch}'", false, $"difftool -d \"{currentBranch}\" \"{_TargetBranch}\""),
                                                SuggestedAction.Verify,
@@ -54,7 +56,7 @@ namespace GitSuggest.SuggestionProviders
             }
             if (ahead > 0)
             {
-                suggestions.Add(new Suggestion(_Rank - 25, $"Feature-branch '{currentBranch}' is {ahead} commit{(ahead != 1 ? "s" : "")} ahead of '{_TargetBranch}'",
+                suggestions.Add(new Suggestion(_Rank - 25, $"{_BranchType} '{currentBranch}' is {ahead} commit{(ahead != 1 ? "s" : "")} ahead of '{_TargetBranch}'",
                                                $"This means that you have commit on '{currentBranch}' that have not been incorporated into '{_TargetBranch}'",
                                                new SuggestedAction($"View a diff between '{currentBranch}' and '{_TargetBranch}'", false, $"difftool -d \"{currentBranch}\" \"{_TargetBranch}\""),
                                                SuggestedAction.Verify,
