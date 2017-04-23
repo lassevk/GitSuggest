75f978b55c47cde51b1fe826be02fae63b82c09b
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-23 17:48:37 +0200

    Added rank to develop links.

--- GitSuggest/SuggestionProviders/Develop66ToDevelopNextSuggestionProvider.cs
+++ GitSuggest/SuggestionProviders/Develop66ToDevelopNextSuggestionProvider.cs
@@ -5,7 +5,7 @@ namespace GitSuggest.SuggestionProviders
     internal class Develop66ToDevelopNextSuggestionProvider : LinkedBranchAheadBehindSuggestionProvider
     {
         public Develop66ToDevelopNextSuggestionProvider()
-            : base(branch => branch == "develop/6.6", (string)"develop/next")
+            : base(branch => branch == "develop/6.6", (string)"develop/next", 200)
         {
         }
     }
