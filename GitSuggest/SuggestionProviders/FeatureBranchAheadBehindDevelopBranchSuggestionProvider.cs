4e9679fdfede96c9a42986707e32341faa4ad6f5
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-23 17:44:14 +0200

    Split classes into separate files

--- GitSuggest/SuggestionProviders/FeatureBranchAheadBehindDevelopBranchSuggestionProvider.cs
+++ GitSuggest/SuggestionProviders/FeatureBranchAheadBehindDevelopBranchSuggestionProvider.cs
@@ -1,77 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
-
-using JetBrains.Annotations;
 
 namespace GitSuggest.SuggestionProviders
 {
-    internal abstract class LinkedBranchAheadBehindSuggestionProvider : ISuggestionProvider
-    {
-        [NotNull]
-        private readonly Predicate<string> _BranchNamePredicate;
-
-        [NotNull]
-        private readonly string _TargetBranch;
-
-        internal LinkedBranchAheadBehindSuggestionProvider([NotNull] Predicate<string> branchNamePredicate, [NotNull] string targetBranch)
-        {
-            _BranchNamePredicate = branchNamePredicate;
-            _TargetBranch = targetBranch;
-            if (branchNamePredicate == null)
-                throw new ArgumentNullException(nameof(branchNamePredicate));
-            if (targetBranch == null)
-                throw new ArgumentNullException(nameof(targetBranch));
-        }
-
-        public async Task<List<Suggestion>> GetSuggestions(GitRepository repository, IConfiguration configuration)
-        {
-            if ((await repository.GetFileStatus()).Count > 0)
-                return new List<Suggestion>();
-
-            var currentBranch = await repository.GetCurrentBranch();
-            if (currentBranch == null || !_BranchNamePredicate(currentBranch))
-                return new List<Suggestion>();
-
-            if (!await repository.BranchExists("develop"))
-                return new List<Suggestion>();
-
-            var (ahead, behind) = await repository.GetAheadBehind(currentBranch, "develop");
-            if (ahead == 0 && behind == 0)
-                return new List<Suggestion>();
-
-            var suggestions = new List<Suggestion>();
-            if (ahead != 0 || behind != 0)
-            {
-                string title = $"Feature-branch '{currentBranch}' has diverged from '{_TargetBranch}'";
-                if (ahead != 0 && behind != 0)
-                    title += $" ({ahead} commit{(ahead != 1 ? "s" : "")} ahead and {behind} behind)";
-                else if (ahead != 0)
-                    title += $" ({ahead} commit{(ahead != 1 ? "s" : "")} ahead)";
-                else
-                    title += $" ({behind} commit{(behind != 1 ? "s" : "")} behind)";
-                suggestions.Add(new Suggestion(600, title,
-                                               "You should analyze the differences before performing the appropriate action",
-                                               new SuggestedAction($"View a diff between '{currentBranch}' and '{_TargetBranch}'", false, $"difftool -d \"{currentBranch}\" \"{_TargetBranch}\"")));
-            }
-            if (behind > 0)
-            {
-                suggestions.Add(new Suggestion(575, $"Feature-branch '{currentBranch}' is {behind} commit{(behind != 1 ? "s" : "")} behind '{_TargetBranch}'",
-                                               $"This means that other changes have been applied to '{_TargetBranch}' that has not yet been incorporated into '{currentBranch}'",
-                                               new SuggestedAction($"Pull from '{_TargetBranch}' into '{currentBranch}'", true, $"pull \"{_TargetBranch}\"")));
-            }
-            if (ahead > 0)
-            {
-                suggestions.Add(new Suggestion(550, $"Feature-branch '{currentBranch}' is {ahead} commit{(ahead != 1 ? "s" : "")} ahead of '{_TargetBranch}'",
-                                               $"This means that you have commit on '{currentBranch}' that have not been incorporated into '{_TargetBranch}'",
-                                               new SuggestedAction($"Check out '{_TargetBranch}' and merge in '{currentBranch}'", true, $"checkout {_TargetBranch}", $"merge {currentBranch}")));
-            }
-
-            return suggestions;
-        }
-    }
-
     internal class FeatureBranchAheadBehindDevelopBranchSuggestionProvider : LinkedBranchAheadBehindSuggestionProvider
     {
         public FeatureBranchAheadBehindDevelopBranchSuggestionProvider()
