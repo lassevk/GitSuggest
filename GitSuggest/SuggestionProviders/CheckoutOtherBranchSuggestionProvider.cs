e177d50e5ae0229f7b5ec17782a583744a999a87
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-23 17:58:10 +0200

    Added low-ranking suggestion to check out another branch.

---
+++ GitSuggest/SuggestionProviders/CheckoutOtherBranchSuggestionProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSuggest.SuggestionProviders
+{
+    internal class CheckoutOtherBranchSuggestionProvider : ISuggestionProvider
+    {
+        public async Task<List<Suggestion>> GetSuggestions(GitRepository repository, IConfiguration configuration)
+        {
+            if ((await repository.GetFileStatus()).Count > 0)
+                return new List<Suggestion>();
+
+            var branch = await repository.GetCurrentBranch();
+            if (branch == null)
+                return new List<Suggestion>();
+
+            var branches = await repository.GetBranches();
+            var actions = new List<SuggestedAction>();
+            foreach (var otherBranch in branches.Where(b => !b.isCurrent).Select(b => b.branchName))
+            {
+                actions.Add(new SuggestedAction("Check out branch '{otherBranch}'", true, $"checkout {otherBranch}"));
+            }
+            var suggestions = new List<Suggestion>();
+            if (actions.Count > 0)
+                suggestions.Add(new Suggestion(100, "You have other branches that you can check out locally", "", actions.ToArray()));
+            return suggestions;
+        }
+    }
+}
