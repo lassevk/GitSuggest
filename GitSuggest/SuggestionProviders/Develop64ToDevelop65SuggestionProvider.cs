71561acf9843d42030999256a6ffe2d0c015f03f
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-04-23 17:43:39 +0200

    Added develop/6.4 -> develop/6.5 -> develop/6.6 -> develop/next suggestion providers

---
+++ GitSuggest/SuggestionProviders/Develop64ToDevelop65SuggestionProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitSuggest.SuggestionProviders
+{
+    internal class Develop64ToDevelop65SuggestionProvider : LinkedBranchAheadBehindSuggestionProvider
+    {
+        public Develop64ToDevelop65SuggestionProvider()
+            : base(branch => branch == "develop/6.4", "develop/6.5")
+        {
+        }
+    }
+}
---
+++ GitSuggest/SuggestionProviders/Develop66ToDevelopNextSuggestionProvider.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GitSuggest.SuggestionProviders
+{
+    internal class Develop66ToDevelopNextSuggestionProvider : LinkedBranchAheadBehindSuggestionProvider
+    {
+        public Develop66ToDevelopNextSuggestionProvider()
+            : base(branch => branch == "develop/6.6", (string)"develop/next")
+        {
+        }
+    }
+}
